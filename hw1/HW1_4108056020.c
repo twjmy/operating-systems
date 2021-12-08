18620530b49dbf204d965c8b519dee290a3e747f
VJ <twjmy@msn.com>
2021-12-08 16:23:49 +0800

    🕞adds: watch Ctrl+Z 80%

--- hw1/HW1_4108056020.c
+++ hw1/HW1_4108056020.c
@@ -7,33 +7,32 @@
 #include <signal.h>
 char *cmd, promp[100]={'\0'}, cwd[100]={'\0'}, buf[100]={'\0'};
 void handler(int);
-struct sigaction sa={
-    .sa_handler = handler,
-    .sa_flags = 0
-}, sa2={
-    .sa_handler = handler,
-    .sa_flags = 0
-};
+struct sigaction internal_c = { .sa_handler = handler, .sa_flags = 0 },
+                 internal_z = { .sa_handler = handler, .sa_flags = 0 },
+                 external_c = { .sa_handler = handler, .sa_flags = 0 },
+                 external_z = { .sa_handler = handler, .sa_flags = 0 };
 int excmd(){
     switch(fork()){
         case -1:{
             perror("Error");
             return EXIT_FAILURE;
-        }case 0:{
-            sigemptyset(&sa2.sa_mask);
-            sigaction(SIGINT, &sa2, NULL);
-            char *argv[100];
-            int argc = 0;
-            for(char *str = strtok(cmd, " "), *arg; str != NULL;){
-                arg = malloc(strlen(str) + 1);
-                if(arg != NULL) strcpy(arg, str);
+        }
+        case 0:{
+            sigemptyset(&external_c.sa_mask);
+            sigemptyset(&external_z.sa_mask);
+            sigaction(SIGINT, &external_c, NULL);
+            sigaction(SIGTSTP, &external_z, NULL);
+            char *argv[100]; int argc = 0;
+            for(char *t = strtok(cmd, " "), *arg; t; t = strtok(NULL, " ")){
+                arg = malloc(strlen(t) + 1);
+                if(arg) strcpy(arg, t);
                 argv[argc++] = arg;
-                str = strtok(NULL, " ");
             }
             argv[argc] = NULL;
             execvp(argv[0], argv);
             return EXIT_SUCCESS;
-        }default:
+        }
+        default:
             wait(NULL);
             return EXIT_SUCCESS;
     }
@@ -41,22 +40,28 @@ int excmd(){
 void handler(int signum){
     puts("");
     switch(signum){
-        case SIGINT:{
+        case SIGINT:{ //puts("SIGINT");
             write_history(".bash_history");
+            printf("kill(%d,%d)", getpid(), SIGTERM);
             kill(getpid(), SIGTERM);
-        }case SIGTSTP:{
-            register HIST_ENTRY **the_list = history_list();
+            return;
+        }
+        case SIGTSTP:{ //puts("SIGTSTP"); //TO-DO
+            register HIST_ENTRY **history = history_list();
             register int i;
-            for(i = 0; the_list[i++];);
-            cmd = the_list[--i]->line;
+            for(i = -1; history[++i];);
+            cmd = history[--i]->line;
+            printf(cmd);
             excmd();
         }
     }
 }
 int main(){
     printf("4108056020 shell ");
-    sigemptyset(&sa.sa_mask);
-    sigaction(SIGINT, &sa, NULL);
+    sigemptyset(&internal_c.sa_mask);
+    sigemptyset(&internal_z.sa_mask);
+    sigaction(SIGINT, &internal_c, NULL);
+    sigaction(SIGTSTP, &internal_z, NULL);
     using_history();
     read_history(".bash_history");
     puts("start");
@@ -78,32 +83,34 @@ int main(){
             setenv(cwd, buf, 1);
             printf("%s=%s\n",cwd,getenv(cwd));
             continue;
-        }else if(sscanf(cmd,"echo %[^\n]\n",buf)>0){
+        }else if(sscanf(cmd,"echo %[^\n]\n", buf)>0){
             puts(buf);
             continue;
         }else if(!strcmp(cmd, "pwd")){
             puts(getcwd(cwd,sizeof(cwd)));
             continue;
-        }else if(sscanf(cmd,"cd %[^\n]\n",buf)>0){
+        }else if(sscanf(cmd,"cd %[^\n]\n", buf)>0){
             chdir(buf);
             continue;
-        }else if(!strcmp(cmd, "history")){
-            register HIST_ENTRY **the_list = history_list();
-            for(register int i = 0; the_list[i]; i++)
-                printf("%d %s\n", i + history_base, the_list[i]->line);
+        }
+        else if(!strcmp(cmd, "history")){
+            register HIST_ENTRY **history = history_list();
+            for(register int i = 0; history[i]; i++)
+                printf("%d %s\n", i + history_base, history[i]->line);
             continue;
         }else if(sscanf(cmd,"history %d", &h)>0 && h>0){
-            register HIST_ENTRY **the_list = history_list();
+            register HIST_ENTRY **history = history_list();
             register int current;
-            for(current = 0; the_list[current++];);
+            for(current = 0; history[current++];);
             if(h > current--) continue;
-            for(register int i = current - h; the_list[i]; i++)
-                printf("%d %s\n", i + history_base, the_list[i]->line);
+            for(register int i = current - h; history[i]; i++)
+                printf("%d %s\n", i + history_base, history[i]->line);
             continue;
-        }else if(!strcmp(cmd, "exit")){
+        }
+        else if(!strcmp(cmd, "exit")){
             write_history(".bash_history");
             return EXIT_SUCCESS;
         }else if(excmd() == EXIT_SUCCESS) continue;
-        // for debug: else perror("Error");
+        //else perror("Error");
     }
 }
